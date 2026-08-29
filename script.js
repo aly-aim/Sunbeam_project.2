fab872593400111236602aa5d2202e53d273748a
alaracodes <sezeralara@gmail.com>
2026-08-29 19:10:54 +0000

    first page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("firstCatBTN").addEventListener("click", function() {
+    document.getElementById("firstCatDiv").style.display = "block"; 
+    document.getElementById("firstCatBTN").style.display = "none";
+}); 
+document. getElementById("secondCatButton").addEventListener("click", function() {
+    document.getElementById("secondCatDiv").style.display = "block"; 
+    document.getElementById("secondCatButton").style.display = "none";
+});
